dd6d1460917756ebc4ce0eb1af2ad8716d9ac8c5
msmkdenis <msmkdenis@gmail.com>
2024-01-12 16:35:13 +0300

    fix: review - changed require to assert

--- main_test.go
+++ main_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	_ "modernc.org/sqlite"
@@ -39,26 +40,30 @@ func TestSuite(t *testing.T) {
 func (s *SQLiteSuite) Test_SelectClient_WhenOk() {
 	clientID := 1
 	s.T().Run("Successful select", func(t *testing.T) {
+		// Получить объект клиента функцией selectClient(), если функция вернула ошибку — завершить тест.
 		client, err := selectClient(s.db, clientID)
 		require.NoError(t, err)
-		require.Equal(t, clientID, client.ID)
-		require.NotEmpty(t, client.FIO)
-		require.NotEmpty(t, client.Login)
-		require.NotEmpty(t, client.Birthday)
-		require.NotEmpty(t, client.Email)
+
+		assert.Equal(t, clientID, client.ID)
+		assert.NotEmpty(t, client.FIO)
+		assert.NotEmpty(t, client.Login)
+		assert.NotEmpty(t, client.Birthday)
+		assert.NotEmpty(t, client.Email)
 	})
 }
 
 func (s *SQLiteSuite) Test_SelectClient_WhenNoClient() {
 	clientID := -1
 	s.T().Run("Fail when no client", func(t *testing.T) {
+		// Проверить, что функция вернула ошибку и ошибка равна sql.ErrNoRows. Иначе завершить тест.
 		client, err := selectClient(s.db, clientID)
 		require.Equal(t, err, sql.ErrNoRows)
-		require.Empty(t, client.ID)
-		require.Empty(t, client.FIO)
-		require.Empty(t, client.Login)
-		require.Empty(t, client.Birthday)
-		require.Empty(t, client.Email)
+
+		assert.Empty(t, client.ID)
+		assert.Empty(t, client.FIO)
+		assert.Empty(t, client.Login)
+		assert.Empty(t, client.Birthday)
+		assert.Empty(t, client.Email)
 	})
 }
 
@@ -70,18 +75,22 @@ func (s *SQLiteSuite) Test_InsertClient_ThenSelectAndCheck() {
 		Email:    "mail@mail.com",
 	}
 	s.T().Run("Successful insert and then select", func(t *testing.T) {
+		// Проверить, что функция вернула не пустой идентификатор и пустую ошибку. Иначе завершить тест.
 		id, err := insertClient(s.db, cl)
 		cl.ID = id
 		require.NotEmpty(t, id)
 		require.NoError(t, err)
 
+		// Функцией selectClient() получить объект Client по идентификатору.
+		// Проверить, что функция вернула пустую ошибку. Иначе завершить тест.
 		client, err := selectClient(s.db, id)
 		require.NoError(t, err)
-		require.Equal(t, cl.ID, client.ID)
-		require.Equal(t, cl.FIO, client.FIO)
-		require.Equal(t, cl.Login, client.Login)
-		require.Equal(t, cl.Birthday, client.Birthday)
-		require.Equal(t, cl.Email, client.Email)
+
+		assert.Equal(t, cl.ID, client.ID)
+		assert.Equal(t, cl.FIO, client.FIO)
+		assert.Equal(t, cl.Login, client.Login)
+		assert.Equal(t, cl.Birthday, client.Birthday)
+		assert.Equal(t, cl.Email, client.Email)
 	})
 }
 
@@ -93,16 +102,21 @@ func (s *SQLiteSuite) Test_InsertClient_DeleteClient_ThenCheck() {
 		Email:    "mail@mail.com",
 	}
 	s.T().Run("Successful insert, delete and then check deleted", func(t *testing.T) {
+		// Проверить, что функция вернула не пустой идентификатор и пустую ошибку. Иначе завершить тест.
 		id, err := insertClient(s.db, cl)
 		require.NotEmpty(t, id)
 		require.NoError(t, err)
 
+		// Получить объект клиента функцией selectClient(). Если функция вернула ошибку, завершить тест.
 		client, err := selectClient(s.db, id)
 		require.NoError(t, err)
 
+		// Удалить запись функцией deleteClient(). Если функция вернула ошибку, завершить тест.
 		err = deleteClient(s.db, client.ID)
 		require.NoError(t, err)
 
+		// Получить объект клиента функцией selectClient().
+		// Проверить, что функция вернула ошибку и ошибка равна sql.ErrNoRows. Иначе завершить тест.
 		_, err = selectClient(s.db, client.ID)
 		require.Equal(t, err, sql.ErrNoRows)
 	})
